8c3148d1ff3bc4ea8bcb93f26a09586a35be3b1e
hollvnt <mark05200219@gmail.com>
2022-11-23 18:26:46 +0300

    add db and fix main page

--- src/test/java/com/yaskovich/BuysellApplicationTests.java
+++ src/test/java/com/yaskovich/BuysellApplicationTests.java
@@ -1,13 +1,13 @@
-package by.yaskovich.java_project;
+package com.yaskovich;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaProjectApplicationTests {
+class BuysellApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
